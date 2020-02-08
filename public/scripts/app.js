f38d5df7cb96a7efb5161f599cbc451630a7be7d
Chris Luksatrio <ranggaluksatrio@gmail.com>
2020-02-08 17:25:18 +0000

    Add index page with button and empty game page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,5 @@
 $(() => {
+
   $.ajax({
     method: "GET",
     url: "/api/users"
@@ -7,4 +8,22 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+
 });
+
+$(document).ready(() => {
+
+  const socket = new WebSocket('ws://localhost:8080');
+
+  $( ".start-button" ).on('click', function(event) {
+    socket.addEventListener('open', function (event) {
+        socket.send('Hello Server!');
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
+    });
+  })
+
+})
