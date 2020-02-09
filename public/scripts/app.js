42cb5095b47a9e9fb925e51eec93204e5fab6b75
JehyunRa <jra4862@gmail.com>
2020-02-09 00:12:12 +0000

    modified message display to console

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,3 +10,24 @@
 //   });;
 
 // });
+<<<<<<< dd6c49b68cf2849393c37d57e4d61a5c4cdd4f3b
+=======
+
+$(document).ready(() => {
+
+
+
+  // const socket = new WebSocket('ws://localhost:8080');
+
+  // $( ".start-button" ).on('click', function(event) {
+  //   socket.addEventListener('open', function (event) {
+  //       socket.send('Hello Server!');
+  //   });
+
+  //   // Listen for messages
+  //   socket.addEventListener('message', function (event) {
+  //       console.log('Message from server ', event.data);
+  //   });
+
+})
+>>>>>>> modified message display to console
