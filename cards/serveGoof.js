a002eb4aa8f80c5f8715eb882a38321cd77288c3
cspraggett <cspraggett@gmail.com>
2020-02-09 14:00:00 -0500

    set up dummy play

--- cards/serveGoof.js
+++ cards/serveGoof.js
@@ -5,9 +5,25 @@ shuffle(player1.hand);
 shuffle(player2.hand);
 // console.log(player1.hand, player2.hand);
 
-// let handNumber = 0;
+let handNumber = 0;
+player1.currentBid = player1.hand[handNumber];
+player2.currentBid = player2.hand[handNumber];
+dealer.currentCard = dealer.hand[handNumber];
+compareHands(player1, player2, dealer);
+console.log('hand#', handNumber);
+console.log('player1:', player1);
+console.log('player2:', player2);
+console.log('dealer:', dealer);
 
-
+handNumber = 1;
+player1.currentBid = player1.hand[handNumber];
+player2.currentBid = player2.hand[handNumber];
+dealer.currentCard = dealer.hand[handNumber];
+compareHands(player1, player2, dealer);
+console.log('hand#', handNumber);
+console.log('player1:', player1);
+console.log('player2:', player2);
+console.log('dealer:', dealer);
 // (async function loop() {
 //   for (let handNumber = 0; handNumber < 13; handNumber++) {
 //     player1.currentBid = player1.hand[handNumber];
