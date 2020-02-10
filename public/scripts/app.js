279cffcb5060bb5f1102b1a2aa8d38d8e57d4597
ChristoLuksatrio <ranggaluksatrio@gmail.com>
2020-02-10 16:05:33 -0500

    Created header partial, login page, sign up page, landing page (#11)
    * Add index page with button and empty game page
    
    * create game page
    
    * make game page responsive with desktop width
    
    * fix card responsiveness
    
    * fix merge conflict
    
    * added navbar
    
    * Create header partial, login page, sign up page, landing page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,30 @@
+$(() => {
+
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($("body"));
+    }
+  });;
+
+});
+
+$(document).ready(() => {
+
+  const socket = new WebSocket('ws://localhost:8080');
+
+  $( ".start-button" ).on('click', function(event) {
+    socket.addEventListener('open', function (event) {
+        socket.send('Hello Server!');
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
+    });
+  })
 // $(() => {
 
 //   $.ajax({
