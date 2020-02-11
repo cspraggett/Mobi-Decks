77647c3c16d3abbeefb5ce2c790f26aafbe0f968
cspraggett <cspraggett@gmail.com>
2020-02-11 15:44:03 -0500

    make translate function

--- cards/crazy8s.js
+++ cards/crazy8s.js
@@ -20,6 +20,69 @@ class game {
     this.faceUpCard = null;
   }
 
+  translateCard(card) {
+    const translater = {
+      0: {value: 'A', suit: 'C'},
+      1: {value: '2', suit: 'C'},
+      2: {value: '3', suit: 'C'},
+      3: {value: '4', suit: 'C'},
+      4: {value: '5', suit: 'C'},
+      5: {value: '6', suit: 'C'},
+      6: {value: '7', suit: 'C'},
+      7: {value: '8', suit: 'C'},
+      8: {value: '9', suit: 'C'},
+      9: {value: '10', suit: 'C'},
+      11: {value: 'Q', suit: 'C'},
+      12: {value: 'K', suit: 'C'},
+      10: {value: 'J', suit: 'C'},
+      13: {value: 'A', suit: 'S'},
+      14: {value: '2', suit: 'S'},
+      15: {value: '3', suit: 'S'},
+      16: {value: '4', suit: 'S'},
+      17: {value: '5', suit: 'S'},
+      18: {value: '6', suit: 'S'},
+      19: {value: '7', suit: 'S'},
+      20: {value: '8', suit: 'S'},
+      21: {value: '9', suit: 'S'},
+      22: {value: '10', suit: 'S'},
+      23: {value: 'J', suit: 'S'},
+      24: {value: 'Q', suit: 'S'},
+      25: {value: 'K', suit: 'S'},
+      26: {value: 'A', suit: 'H'},
+      27: {value: '2', suit: 'H'},
+      28: {value: '3', suit: 'H'},
+      29: {value: '4', suit: 'H'},
+      30: {value: '5', suit: 'H'},
+      31: {value: '6', suit: 'H'},
+      32: {value: '7', suit: 'H'},
+      33: {value: '8', suit: 'H'},
+      34: {value: '9', suit: 'H'},
+      35: {value: '10', suit: 'H'},
+      36: {value: 'J', suit: 'H'},
+      37: {value: 'Q', suit: 'H'},
+      38: {value: 'K', suit: 'H'},
+      39: {value: 'A', suit: 'D'},
+      40: {value: '2', suit: 'D'},
+      41: {value: '3', suit: 'D'},
+      42: {value: '4', suit: 'D'},
+      43: {value: '5', suit: 'D'},
+      44: {value: '6', suit: 'D'},
+      45: {value: '7', suit: 'D'},
+      46: {value: '8', suit: 'D'},
+      47: {value: '9', suit: 'D'},
+      48: {value: '10', suit: 'D'},
+      49: {value: 'J', suit: 'D'},
+      50: {value: 'Q', suit: 'D'},
+      51: {value: 'K', suit: 'D'}
+    };
+    return translater[card];
+  }
+
+  checkIfCardValid(card) {
+    return this.translateCard(card).value === this.translateCard(this.faceUpCard).suit ||
+     this.translateCard(card).suit === this.translateCard(this.faceUpCard).suit;
+  }
+
   takeTopCard(player) {
     player.recieveCards(this.deck.deal(1));
   }
@@ -27,6 +90,8 @@ class game {
   pickUp2(player) {
     player.recieveCards(this.deck.deal(2));
   }
+
+
 }
 const dealer = new game();
 console.log(dealer);
@@ -45,3 +110,8 @@ dealer.pickUp2(player1);
 console.log(player1);
 console.log(dealer);
 console.log(dealer.deck._deck.length);
+
+for (let i = 0; i < player1._hand.length; i++) {
+  console.log(dealer.translateCard(player1._hand[i]));
+}
+
