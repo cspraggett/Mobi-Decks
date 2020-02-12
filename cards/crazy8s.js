a59faf4d3574e34e303e3498cb49e2b295c1e38c
cspraggett <cspraggett@gmail.com>
2020-02-12 12:47:34 -0500

    finish crazy8s alpha version

--- cards/crazy8s.js
+++ cards/crazy8s.js
@@ -31,7 +31,7 @@ class game {
   }
 
   changeFaceUpCard(card) {
-    this.faceUpCard.shift(card);
+    this.faceUpCard.unshift(card);
   }
 
   addToDeck() {
@@ -128,9 +128,20 @@ class game {
     player.recieveCards(this.deck.deal(num));
   }
 
+  moveFaceUpToDeck() {
+    this.deck = this.faceUpCard.filter(value => this.faceUpCard[0] !== value);
+  }
+
   makeMove(frontEndObject) {
+    if (this.deck._deck.length === 0) {
+      this.moveFaceUpToDeck();
+    }
     if (frontEndObject.pickUp) {
       this.takeTopCard(this.players[frontEndObject.player]);
+      if (!this.checkIfMove(this.players[frontEndObject[player]])) {
+        this.currentPlayer = ((frontEndObject.player + 1) % 2);
+        return;
+      }
     }
     if (this.checkIfCardValid(frontEndObject.cards[0])) {
       if (this.translateCard(frontEndObject.card[0]).value === '2') {
