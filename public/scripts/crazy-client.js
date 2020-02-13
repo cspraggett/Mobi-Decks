486bf353506fb5e9795f35438b1fe453940c7854
JehyunRa <jra4862@gmail.com>
2020-02-13 19:54:12 +0000

    middle of making crazy-client

--- public/scripts/crazy-client.js
+++ public/scripts/crazy-client.js
@@ -45,37 +45,50 @@ $(function () {
 
   });
 
-  // display current dealer card
-  const dealerPlay = function(card) {
-    $('.dealer-bid').append($((cardImage.club[card])));
-    data.player.ready = true;
-  }
+  // display current face up card
+  const placeFaceUpCard = function(card) {
+    $('.place-pile').empty();
+    console.log(translateCard(card[0][0]).suit);
+    console.log(card[0][0] % 13);
+    $('.place-pile').append($(
+      cardImage
+      [translateCard(card[0][0]).suit]
+      [card[0][0] % 13]
+    ))
+  };
 
   // place cards at the beginning at the start of a game
   const spawnCardsWithDelay = function(index) {
-    if (index < 13) {
+    if (index < 8) {
       setTimeout(() => {
         let num = index.toString();
-        let playerColor, opponentColor = "";
-        // assign preset colors(suits) for each player
-        if (data.player._id === 1) {
-          playerColor = "spade";
-          opponentColor = "heart";
-        } else if (data.player._id === 2) {
-          playerColor = "heart";
-          opponentColor = "spade";
+        let playerCards = [];
+        let opponentCards = [];
+        if (data.playerNum === 0) {
+          playerCards = data.crazy8.players['0']._hand;
+          opponentCards = data.crazy8.players['1']._hand;
+        } else {
+          playerCards = data.crazy8.players['1']._hand;
+          opponentCards = data.crazy8.players['0']._hand;
         }
+
         // make new divs and place image inside
-        let innerDivTop = $(`<div class="cards">`).append($(cardImage[opponentColor][num]));
-        let innerDivBot = $(`<div class="cards bot">`).append($(cardImage[playerColor][num]));
+        let innerDivTop = $(`<div class="cards">`).append($(
+          cardImage[translateCard(opponentCards[index]).suit][translateCard(opponentCards[index]).n]
+          ));
+        let innerDivBot = $(`<div class="cards bot">`).append($(
+          cardImage[translateCard(playerCards[index]).suit][translateCard(playerCards[index]).n]
+          ));
         // this assigns a hidden value to div that holds imgs, required to track specific card in handDivs
         $(innerDivTop).attr({
-          value: num,
-          style: `z-index: ${num}`
+          value: translateCard(opponentCards[index]).value,
+          suit: translateCard(opponentCards[index]).suit,
+          style: `z-index: ${index}`
         });
         $(innerDivBot).attr({
-          value: num,
-          style: `z-index: ${num}`
+          value: translateCard(playerCards[index]).value,
+          suit: translateCard(playerCards[index]).suit,
+          style: `z-index: ${index}`
         });
         // push newly made divs(cards) into handDivs, array that holds divs equal to hand cards
         handDivs.opponent.push(innerDivTop);
@@ -87,9 +100,10 @@ $(function () {
         spawnCardsWithDelay(index + 1);
       }, 30);
     } else {
-      dealerPlay(data.dealer._hand[0]);
+      placeFaceUpCard(data.crazy8.faceUpCard);
       data.phase = 1;
       console.log('new gamePhase: phase: ' + data.phase);
+      if (data.playerNum === 0) data.ready = true;
     }
   }
 
@@ -97,6 +111,7 @@ $(function () {
   socket.on('gamePhase', function(msg) {
     data = JSON.parse(msg);
     console.log('new gamePhase: phase: ' + data.phase);
+    console.log(data);
 
     //initialization
     if (data.phase === 0) {
@@ -104,69 +119,69 @@ $(function () {
       handDivs.opponent = [];
       spawnCardsWithDelay(0);
       console.log('initialization');
-    } else if (data.phase < 14) {
-      // at the start of each phase
-      setTimeout(() => {
-        $('.bids').empty();
-        $('.dealer-bid').empty();
-        $('.p2-score').text(' ' + data.oScore);
-        $('.p1-score').text(' ' + data.pScore);
-        dealerPlay(data.dealer._hand[0]);
-      }, 1000);
-    } else if (data.phase === 14) {
-      let innerDivTop = $(`<div>`).append(`<p>opponent won cards: ${data.player._wonBids} </p>`);
-      let innerDivBot = $(`<div>`).append(`<p>player won cards: ${data.opponent._wonBids} </p>`);
-      $($(innerDivTop)).prependTo('.bids');
-      $($(innerDivBot)).appendTo('.bids');
+//     } else if (data.phase < 14) {
+//       // at the start of each phase
+//       setTimeout(() => {
+//         $('.bids').empty();
+//         $('.dealer-bid').empty();
+//         $('.p2-score').text(' ' + data.oScore);
+//         $('.p1-score').text(' ' + data.pScore);
+//         dealerPlay(data.dealer._hand[0]);
+//       }, 1000);
+//     } else if (data.phase === 14) {
+//       let innerDivTop = $(`<div>`).append(`<p>opponent won cards: ${data.player._wonBids} </p>`);
+//       let innerDivBot = $(`<div>`).append(`<p>player won cards: ${data.opponent._wonBids} </p>`);
+//       $($(innerDivTop)).prependTo('.bids');
+//       $($(innerDivBot)).appendTo('.bids');
     }
   })
 
-  // when game update information is received
-  socket.on('gameUpdate:goof', function(msg){
-  const update = JSON.parse(msg);
+//   // when game update information is received
+//   socket.on('gameUpdate:goof', function(msg){
+//   const update = JSON.parse(msg);
 
-    if (data.player._id !== update.player * 1) {
-      if (update.item === "bid") {
-        // find location of opponent card that is sent to bid
-        cardValue = update.value;
-        cardIndex = jQuery.inArray(cardValue * 1, data.opponent._hand);
+//     if (data.player._id !== update.player * 1) {
+//       if (update.item === "bid") {
+//         // find location of opponent card that is sent to bid
+//         cardValue = update.value;
+//         cardIndex = jQuery.inArray(cardValue * 1, data.opponent._hand);
 
-        // remove opponent card from hand in data
-        data.opponent._hand.splice(cardIndex, 1);
-        // update opponent current bid in data
-        data.opponent._currentBid = update.value;
-        // place opponent card in bid
-        for (let i = 0; i < handDivs.opponent.length; i++) {
-          if (handDivs.opponent[i].attr("value") === cardValue) {
-            $(handDivs.opponent[i]).prependTo('.bids');
-            $(handDivs.opponent[i]).removeClass('cards');
-            $(handDivs.opponent[i]).addClass('row bid-card');
-          }
-        }
-        console.log('opponent bid: ' + data.opponent._currentBid);
-      }
-    } else {
-      if (update.item === "bid") {
-        // find location of opponent card that is sent to bid
-        cardValue = update.value;
-        cardIndex = jQuery.inArray(cardValue * 1, data.player._hand);
+//         // remove opponent card from hand in data
+//         data.opponent._hand.splice(cardIndex, 1);
+//         // update opponent current bid in data
+//         data.opponent._currentBid = update.value;
+//         // place opponent card in bid
+//         for (let i = 0; i < handDivs.opponent.length; i++) {
+//           if (handDivs.opponent[i].attr("value") === cardValue) {
+//             $(handDivs.opponent[i]).prependTo('.bids');
+//             $(handDivs.opponent[i]).removeClass('cards');
+//             $(handDivs.opponent[i]).addClass('row bid-card');
+//           }
+//         }
+//         console.log('opponent bid: ' + data.opponent._currentBid);
+//       }
+//     } else {
+//       if (update.item === "bid") {
+//         // find location of opponent card that is sent to bid
+//         cardValue = update.value;
+//         cardIndex = jQuery.inArray(cardValue * 1, data.player._hand);
 
-        // remove opponent card from hand in data
-        data.player._hand.splice(cardIndex, 1);
-        // update opponent current bid in data
-        data.player._currentBid = update.value;
-        // place opponent card in bid
-        for (let i = 0; i < handDivs.player.length; i++) {
-          if (handDivs.player[i].attr("value") === cardValue) {
-            $(handDivs.player[i]).appendTo('.bids');
-            $(handDivs.player[i]).removeClass('cards bot');
-            $(handDivs.player[i]).addClass('row bid-card');
-          }
-        }
-        console.log('player bid: ' + data.player._currentBid);
-      }
-    }
-  })
+//         // remove opponent card from hand in data
+//         data.player._hand.splice(cardIndex, 1);
+//         // update opponent current bid in data
+//         data.player._currentBid = update.value;
+//         // place opponent card in bid
+//         for (let i = 0; i < handDivs.player.length; i++) {
+//           if (handDivs.player[i].attr("value") === cardValue) {
+//             $(handDivs.player[i]).appendTo('.bids');
+//             $(handDivs.player[i]).removeClass('cards bot');
+//             $(handDivs.player[i]).addClass('row bid-card');
+//           }
+//         }
+//         console.log('player bid: ' + data.player._currentBid);
+//       }
+//     }
+//   })
 
   //-------------------------------------------------------/
   // document ready events --------------------------------/
@@ -174,14 +189,21 @@ $(function () {
 
   // when player picks a card
   $(".p1-hand").on('click', function(event) {
+    console.log('data.ready: ', data.ready);
     if (
-      data.player._currentBid === null
-      && data.player.ready === true
+      data.ready === true
       && $(event.target.parentNode).attr("value") !== undefined
       ) {
-      cardValue = $(event.target.parentNode).attr("value");
-      socket.emit('gameUpdate:goof', `{"room_id": "${room_id}", "player": "${data.player._id}", "item": "bid", "value": "${cardValue}" }`);
-      data.player.ready = false;
+      console.log($(event.target.parentNode).attr("value"), $(event.target.parentNode).attr("suit"));
+
+      socket.emit(
+      'gameUpdate:crazy',
+      `{"room_id": "${room_id}",
+      "player": "${data.playerNum}", "item": "",
+      "value": "${ $(event.target.parentNode).attr("value") }",
+      "suit": "${ $(event.target.parentNode).attr("suit") }"}`
+      );
+      data.ready = false;
     }
   });
 });
